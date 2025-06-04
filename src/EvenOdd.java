0c8e85ef0ef3cf4a831dd798d3e8d56459d1966f
gseelam48 <gseelam48@gmail.com>
2025-06-04 14:50:21 -0500

    Basics

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args){
+        for (int i = 1; i<=100;i++){
+            if(i%2==0) {
+                System.out.println("even" + i);
+            }
+            else {
+                System.out.println("odd" + i);
+            }
+
+        }
+    }
+}
