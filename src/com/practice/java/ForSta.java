c198d0654809a419fd1266d8eebf1bbf5f257197
gseel <gseelam48@gmail.com>
2025-06-30 12:35:56 -0500

    Added ForSta

---
+++ src/com/practice/java/ForSta.java
@@ -0,0 +1,10 @@
+package com.practice.java;
+
+public class ForSta {
+    public static void main(String[] args){
+        for (int i=2;i<=4 ;i++)
+        {
+            System.out.println("hi"+i);
+        }
+    }
+}
