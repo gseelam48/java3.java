655d2475b6dff3d9eb57132638209c5e46003d0d
gseel <gseelam48@gmail.com>
2025-06-30 11:31:10 -0500

    Added EvenorOdd programm

---
+++ src/com/practice/java/EvenorOdd.java
@@ -0,0 +1,7 @@
+package com.practice.java;
+
+public class EvenorOdd {
+}
+// EmployerDetails pascal classes
+// employerDetails camel  methods ,variables
+//employer_details snake case database table names column names
