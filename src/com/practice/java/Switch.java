e01611f122bae31bb0d4f4b6d30a45c7b9665f88
gseel <gseelam48@gmail.com>
2025-06-30 12:33:45 -0500

    Added Switch

---
+++ src/com/practice/java/Switch.java
@@ -0,0 +1,28 @@
+package com.practice.java;
+
+public class Switch {
+    public static void main(String[] args){
+        int i = 7;
+        switch(i){
+            case 1 :
+                System.out.println("monday");
+                break;
+            case 2 :
+                System.out.println("tuesady");
+                break;
+            case 3 :
+                System.out.println("wednesday");
+                break;
+            case 4 :
+                System.out.println("thursady");
+                break;
+            case 5 :
+                System.out.println("sun");
+                break;
+            default :
+                System.out.println("enter a valid number");
+
+
+        }
+    }
+}
