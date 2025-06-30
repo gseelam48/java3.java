43b3bb8f538b2dd9a70abed73a3b82bc10c60be3
gseel <gseelam48@gmail.com>
2025-06-30 12:37:22 -0500

    Added DataTypes

---
+++ src/com/practice/java/DataTypes.java
@@ -0,0 +1,24 @@
+package com.practice.java;
+
+public class DataTypes {
+    public static void main(String[] args){
+        byte b = 10;
+        short s = 100;
+        int i = 10000;
+        long l = 10_00_00_000L;
+        float f = 16.8f;
+        double d = 3.16;
+        char c = 'c';
+        boolean bo = true;
+
+        System.out.println("byte b:" +b);
+        System.out.println("short s:" +s);
+        System.out.println("int i:" +i);
+        System.out.println("long l:" +l);
+        System.out.println("float f:" +f);
+        System.out.println("double d:" +d);
+        System.out.println("char c" +c);
+        System.out.println("boolean b:" +bo);
+    }
+}
+
