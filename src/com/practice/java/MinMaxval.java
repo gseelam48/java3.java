c605e2895bf2cd0b39c9933174ba588638623721
gseel <gseelam48@gmail.com>
2025-06-30 12:14:09 -0500

    Added MinMaxval

---
+++ src/com/practice/java/MinMaxval.java
@@ -0,0 +1,8 @@
+package com.practice.java;
+
+public class MinMaxval {
+    public static void main(String[] args){
+        System.out.println("byte min:" +Byte.MIN_VALUE +",max=" +Byte.MAX_VALUE);
+        System.out.println("short min:" +Short.MIN_VALUE +"max" + Short.MAX_VALUE);
+    }
+}
