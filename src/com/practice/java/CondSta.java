80e861aafdbab5940298c56024028fe324c9ab4e
gseel <gseelam48@gmail.com>
2025-06-30 12:31:42 -0500

    Added CondSta

---
+++ src/com/practice/java/CondSta.java
@@ -0,0 +1,15 @@
+package com.practice.java;
+
+public class CondSta {
+    public static void main(String[] args){
+        int x = 8;
+        int y = 9;
+        int z = 10;
+        if(x>z||z>x)
+            System.out.println(x);
+        else if(y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+    }
+}
