64450c99d37e2ec33efed8a026cd900afbbc892f
gseel <gseelam48@gmail.com>
2025-06-30 11:35:46 -0500

    Added FizzBuzzexample

---
+++ src/com/practice/java/FizzBuzzexample.java
@@ -0,0 +1,21 @@
+package com.practice.java;
+
+public class FizzBuzzexample {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 10; i++) {
+                if (i > 10) {
+                    System.out.println("Fizz Buzz");
+                } else if (i % 2 == 0) {
+                    System.out.println(i + " - Fizz");
+                } else {
+                    System.out.println(i + " - Buzz");
+                }
+            }
+            int outOfRange = 11;
+            if (outOfRange > 10) {
+                System.out.println(outOfRange + " - Fizz Buzz");
+            }
+        }
+    }
+
+
