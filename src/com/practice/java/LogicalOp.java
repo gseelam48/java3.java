d15f1bbe8f31732fc7e6c16b3f75b260dcf6a292
gseel <gseelam48@gmail.com>
2025-06-30 12:32:27 -0500

    Added LogicalOp

---
+++ src/com/practice/java/LogicalOp.java
@@ -0,0 +1,12 @@
+package com.practice.java;
+
+public class LogicalOp {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 9;
+        int a = 12;
+        int b = 8;
+        boolean result = x>y || a<b;
+        System.out.println(result);
+    }
+}
