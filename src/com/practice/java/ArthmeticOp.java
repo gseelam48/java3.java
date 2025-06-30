a3842011c61d1ccc8cad1d0537d7a8f9ed4a1cf9
gseel <gseelam48@gmail.com>
2025-06-30 12:24:39 -0500

    Added ArthmeticOp

---
+++ src/com/practice/java/ArthmeticOp.java
@@ -0,0 +1,11 @@
+package com.practice.java;
+
+public class ArthmeticOp {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 8;
+        System.out.println("add" +( num1 + 8));
+        System.out.println("sub" +( num2 - 9));
+        System.out.println("modules" +( num2 % 1));
+    }
+}
