a3f9d8e190beceaeef3dbb10e2e05de6f114b3e1
gseel <gseelam48@gmail.com>
2025-06-30 19:09:10 -0500

    Added Substr

---
+++ src/com/practice/java/Substr.java
@@ -0,0 +1,9 @@
+package com.practice.java;
+
+public class Substr {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 8;
+        System.out.println("sub"+(a-b));
+    }
+}
